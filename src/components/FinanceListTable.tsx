82be58f3832c3fa37caef45d87028f147c741200
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-18 22:58:53 -0300

    feat: add delete finance, and style text

--- src/components/FinanceListTable.tsx
+++ src/components/FinanceListTable.tsx
@@ -1,16 +1,14 @@
 import { FinanceList } from '@/types/finance-list'
 import { currencyFormat } from '@/app/lib/currencyFormat'
+import { Trash2 } from 'lucide-react'
+import { twMerge } from 'tailwind-merge'
 
-export function FinanceListTable({
-  financeList,
-}: {
+interface Props {
   financeList?: FinanceList[]
-}) {
-  function getValueColor(finance: FinanceList) {
-    const color = finance.type === 'withdraw' ? 'red' : 'green'
-    return `text-${color}-500`
-  }
+  onDelete: (value: string) => void
+}
 
+export function FinanceListTable({ financeList, onDelete }: Props) {
   if (!financeList) return null
 
   return (
@@ -23,23 +21,34 @@ export function FinanceListTable({
       </div>
 
       {financeList?.length > 0 ? (
-        financeList?.map((finance) => (
-          <div
-            key={finance.id}
-            className="grid h-10 w-[820px] grid-cols-4 items-center gap-4 rounded-lg bg-white shadow-xl"
-          >
-            <p className="ml-5 text-start font-medium">{finance.title}</p>
-            <p className={`text-start font-medium ${getValueColor(finance)}`}>
-              {currencyFormat(finance.amount)}
-            </p>
-            <p className="text-start font-medium">{finance.category}</p>
-            <div className="ml-5 text-start font-medium">
-              {new Date(finance.date).toLocaleDateString()}
+        financeList?.map((finance) => {
+          const textColorClassName = twMerge(
+            'text-start font-medium',
+            finance.type === 'withdraw' ? `text-red-500` : 'text-green-500',
+          )
 
-              <button>Delete</button>
+          return (
+            <div
+              key={finance.id}
+              className="grid h-10 w-[820px] grid-cols-4 items-center gap-4 rounded-lg bg-white shadow-xl"
+            >
+              <p className="ml-5 text-start font-medium">{finance.title}</p>
+              <p className={textColorClassName}>
+                {currencyFormat(finance.amount)}
+              </p>
+              <p className="text-start font-medium">{finance.category}</p>
+              <div className="mx-5 flex items-center justify-between text-start font-medium">
+                {new Date(finance.date).toLocaleDateString()}
+                <button
+                  className="text-red-500 transition-all hover:text-red-400"
+                  onClick={() => onDelete(finance.id)}
+                >
+                  <Trash2 />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
+          )
+        })
       ) : (
         <div className="mt-5 text-lg font-bold">
           Adicione uma nova transação
