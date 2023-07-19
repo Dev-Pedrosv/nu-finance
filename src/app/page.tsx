9f5a014917d84ac29d4773e6c5f12742b3963d5c
Pedro Silva <pedro_rike@hotmail.com.br>
2023-07-19 20:47:49 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import NuBankLogo from '@/assets/logo.svg'
+import GoogleLogo from '@/assets/google-icon.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="h-screen bg-[#E5E5E5]">
-      <h1>Login</h1>
+    <main className="flex h-screen flex-col items-center justify-center gap-8 bg-[#E5E5E5]">
+      <div className="relative h-[80px] w-[150px]">
+        <Image src={NuBankLogo} alt="nubank logo" fill />
+      </div>
+      <button className="flex gap-2 rounded-2xl bg-purple-600 px-6 py-2 font-semibold text-white">
+        <div className="relative h-6 w-6 ">
+          <Image src={GoogleLogo} alt="google logo" fill />
+        </div>
+        Login com Google
+      </button>
     </main>
   )
 }
